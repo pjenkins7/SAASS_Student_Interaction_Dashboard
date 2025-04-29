62e9d3bc55886ee94e1099f934398f10c7a36a58
pjenkins7 <88481007+pjenkins7@users.noreply.github.com>
2025-04-29 15:59:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sns 
 
 # Configure Streamlit
 st.set_page_config(layout="wide")
